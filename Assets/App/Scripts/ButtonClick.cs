3203906dea975808c2e077d7a003e8f552eec288
Joost van Schaik <joostvanschaik@outlooik.com>
2017-12-26 18:03:08 +0100

    Initial source commit

---
+++ Assets/App/Scripts/ButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkitExtensions.Audio;
+using HoloToolkitExtensions.Messaging;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Messenger.Instance.Broadcast(
+            new SpokenFeedbackMessage { Message = "Thank you for pressing this button"});
+    }
+}
---
+++ Assets/App/Scripts/SpeechCommandExecuter.cs
@@ -0,0 +1,11 @@
+﻿using HoloToolkitExtensions.Audio;
+using HoloToolkitExtensions.Messaging;
+using UnityEngine;
+
+public class SpeechCommandExecuter : MonoBehaviour
+{
+    public void ExecuteTestCommand()
+    {
+        Messenger.Instance.Broadcast(new ConfirmSoundMessage());
+    }
+}
